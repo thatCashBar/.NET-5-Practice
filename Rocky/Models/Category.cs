1e493cb3e29ef41bf94ea226d06a2088a7077625
Jaxon Ray Petersen <thatCashBar@gmail.com>
2021-07-25 18:43:31 -0400

    Add project files.

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
