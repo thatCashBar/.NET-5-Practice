e522dc57ce03a0cd62b636f616ff519e0912e4e5
Jaxon Ray Petersen <thatCashBar@gmail.com>
2021-11-01 21:36:12 -0400

    Added FontAwesome Icons and Added ability to Delete and Edit Categories

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Rocky.Controllers
 {
@@ -41,6 +38,50 @@ namespace Rocky.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            var obj = _db.Category.Find(id);
+            if (obj == null) return NotFound();
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            var obj = _db.Category.Find(id);
+            if (obj == null) return NotFound();
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null) return NotFound();
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
