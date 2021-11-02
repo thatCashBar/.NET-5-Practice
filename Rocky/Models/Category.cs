ad4138ddf523ec8802f3d8bf8df232afb937b9fb
Jaxon Ray Petersen <thatCashBar@gmail.com>
2021-11-01 20:47:34 -0400

    Updated Category Model and CategoryController as well as Index and Create Views

--- Rocky/Models/Category.cs
+++ Rocky/Models/Category.cs
@@ -12,9 +12,12 @@ namespace Rocky.Models
         [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         [DisplayName("Display Order")]
+        [Range(1,int.MaxValue,ErrorMessage = "Display Order for Category must be grater than 0")]
         public int DisplayOrder { get; set; }
     }
 }
