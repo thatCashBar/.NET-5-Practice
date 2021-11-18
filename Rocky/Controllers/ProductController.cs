42e1ff8f02f0126870e402daaeecd61dfeb091b5
Jaxon Ray Petersen <thatCashBar@gmail.com>
2021-11-18 13:00:52 -0500

    Create Product functionality added

--- Rocky/Controllers/ProductController.cs
+++ Rocky/Controllers/ProductController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Rocky.Controllers
@@ -9,10 +14,12 @@ namespace Rocky.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(ApplicationDbContext db)
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -30,25 +37,61 @@ namespace Rocky.Controllers
         //GET - UPSERT
         public IActionResult Upsert(int? id)
         {
-            Product product = new Product();
-            if (id == null) return View(product);
-            product = _db.Product.Find(id);
-            if (product == null) return NotFound();
-            return View(product);
+            //IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(i => new SelectListItem
+            //{
+            //    Text = i.Name,
+            //    Value = i.Id.ToString()
+            //});
+
+            //ViewBag.CategoryDropDown = CategoryDropDown;
+
+            //Product product = new Product();
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                Product = new Product(),
+                CategorySelectList = _db.Category.Select(i => new SelectListItem { 
+                    Text = i.Name, Value = i.Id.ToString() 
+                })
+            };
+
+            if (id == null) return View(productViewModel);
+            productViewModel.Product = _db.Product.Find(id);
+            if (productViewModel.Product == null) return NotFound();
+            return View(productViewModel);
         }
 
         //POST - UPSERT
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Product obj)
+        public IActionResult Upsert(ProductViewModel productViewModel)
         {
             if (ModelState.IsValid)
             {
-                _db.Product.Add(obj);
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvironment.WebRootPath;
+
+                if (productViewModel.Product.Id == 0)
+                {
+                    //Creating
+                    string upload = webRootPath + WebConstants.ImagePath;
+                    string fileName = Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+                    productViewModel.Product.Image = fileName + extension;
+                    _db.Product.Add(productViewModel.Product);
+                }
+                else
+                {
+                    //Updating
+
+                }
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(obj);
+            return View(productViewModel);
         }
 
         //GET - DELETE
