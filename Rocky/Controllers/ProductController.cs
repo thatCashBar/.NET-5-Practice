5c5a7947091635dfe1e3ca8638cafeaa42faa832
Jaxon Ray Petersen <thatCashBar@gmail.com>
2021-11-22 18:52:24 -0500

    Added Delete functionality to Product Page

--- Rocky/Controllers/ProductController.cs
+++ Rocky/Controllers/ProductController.cs
@@ -38,15 +38,6 @@ namespace Rocky.Controllers
         //GET - UPSERT
         public IActionResult Upsert(int? id)
         {
-            //IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(i => new SelectListItem
-            //{
-            //    Text = i.Name,
-            //    Value = i.Id.ToString()
-            //});
-
-            //ViewBag.CategoryDropDown = CategoryDropDown;
-
-            //Product product = new Product();
             ProductViewModel productViewModel = new ProductViewModel()
             {
                 Product = new Product(),
@@ -116,6 +107,11 @@ namespace Rocky.Controllers
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
+            productViewModel.CategorySelectList = _db.Category.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
             return View(productViewModel);
         }
 
@@ -123,18 +119,24 @@ namespace Rocky.Controllers
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0) return NotFound();
-            var obj = _db.Product.Find(id);
-            if (obj == null) return NotFound();
-            return View(obj);
+            Product product = _db.Product.Include(u=>u.Category).FirstOrDefault(u=>u.Id==id); //SUPER FANCY: Loads Category and Object(Product) at same time
+            //product.Category = _db.Category.Find(product.CategoryId);
+            if (product == null) return NotFound();
+            return View(product);
         }
 
         //POST - DELETE
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeletePost(int? id)
         {
             var obj = _db.Product.Find(id);
             if (obj == null) return NotFound();
+
+            string upload = _webHostEnvironment.WebRootPath + WebConstants.ImagePath;
+            var oldFile = Path.Combine(upload, obj.Image);
+            if (System.IO.File.Exists(oldFile)) System.IO.File.Delete(oldFile);
+
             _db.Product.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
